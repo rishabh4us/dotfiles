6e5ddda0a3d64f44ad5afe62b372912b2531bb2a
Luca Giovagnoli <lucagiovagnoli250@gmail.com>
2014-12-08 11:40:01 -0800

    install modified and  tried out

--- myvim_install.sh
+++ myvim_install.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
+sudo apt-get install exuberant-ctags
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cd ~
-git init
-git add remote origin https://lucagiovagnoli250@bitbucket.org/lucagiovagnoli250/vim.git 
-git fetch
 vim +PluginInstall +qall
 
 
